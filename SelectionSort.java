da9f736df924586472209bdda8594255297d8469
Deepa Veligandla <deepaveligandla555@gmail.com>
2026-08-04 07:37:13 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,29 @@
+//selection sort  = to find out smallest element in the list
+//public class SelectionSort {
+//
+//	public static void main(String[] args) {
+//		
+//		   int arr[] = {2,4,5,8,4,5,6,7,1,2,3,4};
+//		   for(int i=0;i<arr.length-1;i++) {
+//			   int min = i;
+//			   for(int j=i+1;j<arr.length-1;j++) {
+//				   if(arr[j]<arr[min]) {
+//					   min = j;
+//				   }
+//			   }
+//				   int temp = arr[i];
+//				   arr[i] = arr[min];
+//				   arr[min] = temp;
+//			   }  
+//		   
+//			 for(int i=0;i<arr.length-1;i++) {
+//			   System.out.print(arr[i]+ " ");
+//		   }
+//			}
+//
+//		}
+
+
+	
+
+
